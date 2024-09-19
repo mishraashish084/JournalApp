13cfa2b0c264b2ec60990add9ee7b5b269ed1b8b
Ashish Mishra <mishraashish084@gmail.com>
2024-09-20 01:15:58 +0530

    Modified pom

--- src/main/java/com/ashish/journalApp/repository/JournalEntryRepo.java
+++ src/main/java/com/ashish/journalApp/repository/JournalEntryRepo.java
@@ -2,8 +2,6 @@ package com.ashish.journalApp.repository;
 
 import com.ashish.journalApp.entity.JournalEntry;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface JournalEntryRepo extends MongoRepository<JournalEntry, Long> {
+public interface JournalEntryRepo extends MongoRepository<JournalEntry, String> {
 }
